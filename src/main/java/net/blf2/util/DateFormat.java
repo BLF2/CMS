59409062015133d67727a3ded5e9095213e529f9
BLF2 <blf20822@126.com>
2016-04-01 14:47:30 +0800

    日期格式化类

--- src/main/java/net/blf2/util/DateFormat.java
+++ src/main/java/net/blf2/util/DateFormat.java
@@ -1,7 +1,14 @@
 package net.blf2.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 /**
  * Created by blf2 on 16-3-31.
+ * 格式化时间类
  */
 public class DateFormat {
+    public String getCurrentDateTime(){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        return sdf.format(new Date());
+    }
 }
