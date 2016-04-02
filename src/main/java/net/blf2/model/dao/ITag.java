48315a4cc1c02d1b8155dc6dbea96edfa3ad52c5
BLF2 <blf20822@126.com>
2016-04-02 13:03:59 +0800

    create interface of ITag and implements this interface

---
+++ src/main/java/net/blf2/model/dao/ITag.java
@@ -0,0 +1,17 @@
+package net.blf2.model.dao;
+
+import net.blf2.model.entry.TagInfo;
+
+import java.util.List;
+
+/**
+ * Created by blf2 on 16-4-2.
+ * 分类信息数据库操作接口
+ */
+public interface ITag {
+    public TagInfo insertTagInfo(TagInfo tagInfo);
+    public Boolean deleteTagInfo(TagInfo tagInfo);
+    public Boolean updateTagInfo(TagInfo tagInfo);
+    public TagInfo queryTagInfoByTagId(Integer tagId);
+    public List<TagInfo> queryTagInfoAll();
+}
---
+++ src/main/java/net/blf2/model/dao/Impl/TagDaoImpl.java
@@ -0,0 +1,73 @@
+package net.blf2.model.dao.Impl;
+
+import net.blf2.model.dao.ITag;
+import net.blf2.model.entry.TagInfo;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by blf2 on 16-4-2.
+ * 分类信息数据库操作，实现ITag接口
+ */
+public class TagDaoImpl implements ITag {
+
+    private SessionFactory sessionFactory;
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public TagInfo insertTagInfo(TagInfo tagInfo) {
+        try{
+            this.sessionFactory.getCurrentSession().save(tagInfo);
+        }catch (HibernateException e){
+            return null;
+        }
+        return tagInfo;
+    }
+
+    @Override
+    public Boolean deleteTagInfo(TagInfo tagInfo) {
+        try {
+            this.sessionFactory.getCurrentSession().delete(tagInfo);
+        }catch (HibernateException e){
+            return Boolean.FALSE;
+        }
+        return Boolean.TRUE;
+    }
+
+    @Override
+    public Boolean updateTagInfo(TagInfo tagInfo) {
+        try {
+            this.sessionFactory.getCurrentSession().update(tagInfo);
+        }catch (HibernateException e){
+            return Boolean.FALSE;
+        }
+        return  Boolean.TRUE;
+    }
+
+    @Override
+    public TagInfo queryTagInfoByTagId(Integer tagId) {
+        String hql = "from TagInfo tagInfo where tagInfo.tagId="+tagId;
+        Query query = this.sessionFactory.getCurrentSession().createQuery(hql);
+        if(query.list().size() > 0)
+            return (TagInfo) query.list().get(0);
+        return null;
+    }
+
+    @Override
+    public List<TagInfo> queryTagInfoAll() {
+        String hql = "from TagInfo";
+        Query query = this.sessionFactory.getCurrentSession().createQuery(hql);
+        return query.list();
+    }
+}
