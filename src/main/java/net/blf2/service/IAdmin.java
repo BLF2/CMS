c0a5f3eb338469d3f9fa7bf32c08d5dabb22d121
BLF2 <blf20822@126.com>
2016-04-03 20:27:35 +0800

    create interface of admin and implement it

--- src/main/java/net/blf2/service/IAdmin.java
+++ src/main/java/net/blf2/service/IAdmin.java
@@ -1,7 +1,20 @@
 package net.blf2.service;
 
+import net.blf2.model.entry.ArticleInfo;
+import net.blf2.model.entry.TagInfo;
+import net.blf2.model.entry.UserInfo;
+
+import java.util.List;
+
 /**
  * Created by blf2 on 16-4-3.
+ * 管理员权限设置
  */
-public interface IAdmin {
+public interface IAdmin extends IPrimaryUser{
+    public List<UserInfo> lookUserInfoAll();
+    public Boolean deleteUserInfoByUserId(Integer userId);
+    public List<ArticleInfo> lookArticleInfoAll();
+    public TagInfo addTagInfo(TagInfo tagInfo);
+    public Boolean updateTagInfo(TagInfo tagInfo);
+    public Boolean deleteTagInfoByTagId(Integer tagId);
 }
--- src/main/java/net/blf2/service/Impl/AdminImpl.java
+++ src/main/java/net/blf2/service/Impl/AdminImpl.java
@@ -1,7 +1,44 @@
 package net.blf2.service.Impl;
 
+import net.blf2.model.entry.ArticleInfo;
+import net.blf2.model.entry.TagInfo;
+import net.blf2.model.entry.UserInfo;
+import net.blf2.service.IAdmin;
+
+import java.util.List;
+
 /**
  * Created by blf2 on 16-4-3.
+ * 管理员类的接口实现
  */
-public class AdminImpl {
+public class AdminImpl extends PrimaryUserImpl implements IAdmin  {
+    @Override
+    public List<UserInfo> lookUserInfoAll() {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteUserInfoByUserId(Integer userId) {
+        return null;
+    }
+
+    @Override
+    public List<ArticleInfo> lookArticleInfoAll() {
+        return null;
+    }
+
+    @Override
+    public TagInfo addTagInfo(TagInfo tagInfo) {
+        return null;
+    }
+
+    @Override
+    public Boolean updateTagInfo(TagInfo tagInfo) {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteTagInfoByTagId(Integer tagId) {
+        return null;
+    }
 }
