8c2cf271a8bf150d78a1f40e14aa04535cd8a426
BLF2 <blf20822@126.com>
2016-04-07 21:44:57 +0800

    add class to get web.xml param

--- src/main/java/net/blf2/util/ApplicationContextParam.java
+++ src/main/java/net/blf2/util/ApplicationContextParam.java
@@ -1,7 +1,24 @@
 package net.blf2.util;
 
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
 /**
  * Created by blf2 on 16-4-7.
+ * 获得web.xml中的参数
  */
-public class ApplicationContextParam {
+public class ApplicationContextParam implements ServletContextListener{
+
+    private static WebApplicationContext webApplicationContext;
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        webApplicationContext = (WebApplicationContext) servletContextEvent.getServletContext();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
 }
